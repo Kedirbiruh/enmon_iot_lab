67e92cb238bfcbd1243a1fe6a1bce9cc2eb1795f
Kedir Biruh <kedirbiruh1114@gmail.com>
2026-04-30 15:44:42 +0200

    Erster Commit mit Supabase

---
+++ android/app/src/main/kotlin/com/example/enmon_iot_lab/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.enmon_iot_lab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  final supabaseUrl = dotenv.env['SUPABASE_URL'] ?? '';
+  final supabaseAnonKey = dotenv.env['SUPABASE_ANON_KEY'] ?? '';
+
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseAnonKey);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'EnMon IoT Lab',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('EnMon IoT')),
+        body: const Center(child: Text('IoT')),
+      ),
+    );
+  }
+}
